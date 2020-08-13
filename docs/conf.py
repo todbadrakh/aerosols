174e0d5d95b1f335b980349a33f3e91839c7d929
todbadrakh <togo.odbadrakh@furman.edu>
2020-08-13 19:29:47 -0400

    editing toc

--- docs/conf.py
+++ docs/conf.py
@@ -28,4 +28,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'default'
 html_static_path = ['_static']
-html_sidebars = { '**': ['globaltoc.html','relations.html','sourcelink.html','searchbox.html'] }
+extensions = ['sphinxcontrib-fulltoc']
